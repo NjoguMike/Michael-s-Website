cfc6bd238e9b36fb4b31da8fe1dd8ef6dddea3fc
Michael Njogu <michaelfebzen.mb@gmail.com>
2024-07-18 20:21:27 +0300

    Changed landing

---
+++ script.js
@@ -0,0 +1,16 @@
+// let lastScrollTop
+
+// const navbar = document.getElementById('navbar')
+
+// const moveNavbar = window.addEventListener('scroll',function(){
+//   let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+//   if(scrollTop > lastScrollTop){
+//     navbar.style.top='-80px';
+//   }
+//   else{
+//     navbar.style.top='0';
+//   }
+
+//   lastScrollTop = scrollTop;
+// });
